d4bb723bf81f4a86173cd6a1796a2423d197b5df
Kerrick Chan <kerrickchan@icloud.com>
2024-02-17 12:37:21 +0800

    add RNUI

--- App.tsx
+++ App.tsx
@@ -1,18 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, TextField, Text, Button} from 'react-native-ui-lib';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class Example extends Component {
+
+  render() {
+    return (
+      <View flex paddingH-25 paddingT-120>
+        <Text blue50 text20>Welcome</Text>
+        <TextField text50 placeholder="username" grey10/>
+        <TextField text50 placeholder="password" secureTextEntry grey10/>
+        <View marginT-100 center>
+          <Button text70 white background-orange30 label="Login"/>
+          <Button link text70 orange30 label="Sign Up" marginT-20/>
+        </View>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@components': './components',
+            '@screens': './screens',
+            '@assets': './assets',
+            '@config': './config',
+            '@utils': './utils',
+            '@services': './services',
+            '@store': './store',
+            '@hooks': './hooks',
+            '@navigations': './navigations',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
