69c4b4a6e585311d03f0dad6ada794909648894f
Kerrick Chan <kerrickchan@icloud.com>
2024-02-17 12:47:34 +0800

    add React Redux

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,7 @@ module.exports = function (api) {
             '@utils': './utils',
             '@services': './services',
             '@store': './store',
+            '@slices': './slices',
             '@hooks': './hooks',
             '@navigations': './navigations',
           },
